1750255b5b579a1cc3062c502e1346c95b8210a0
csmcrlw <lidaaaserova@gmail.com>
2023-05-06 22:22:40 +0500

    edit cart models

--- flower_shop/cart/urls.py
+++ flower_shop/cart/urls.py
@@ -6,7 +6,7 @@ router = routers.DefaultRouter()
 router.register(r'cart', views.CartViewSet, basename='cart')
 
 urlpatterns = [
-    path('', include((router.urls, 'flowers_shop.cart'))),
+    path('', include((router.urls, 'flower_shop.cart'))),
     path('user/register/', views.RegisterUser.as_view(), name='user_register'),
     path('user/login/', views.LoginUser.as_view()),
 ]
