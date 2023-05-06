d8de9b1416bbb9e7b6f6c1568d8493fe6c3e8363
csmcrlw <lidaaaserova@gmail.com>
2023-05-06 21:48:31 +0500

    edit shop models

--- flower_shop/shop/models.py
+++ flower_shop/shop/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Название')
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
     class Meta:
@@ -15,13 +15,14 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, blank=True, null=True)
-    name = models.CharField(max_length=200, db_index=True, unique=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE,
+                                 blank=True, null=True, verbose_name='Категория')
+    name = models.CharField(max_length=200, db_index=True, unique=True, verbose_name='Название')
     slug = models.SlugField(max_length=200, db_index=True, default='')
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField(default=0, help_text='Осталось на складе')    # остатки продукта
-    available = models.BooleanField(default=True)
+    description = models.TextField(blank=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    stock = models.PositiveIntegerField(default=0, verbose_name='Осталось на складе')
+    available = models.BooleanField(default=True, verbose_name='Доступно для заказа')
 
     class Meta:
         ordering = ('name', )
